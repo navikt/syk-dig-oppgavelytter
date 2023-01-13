ddf5af6d465775036458632b773163f05551594c
Tia Firing <tia.firing@nav.no>
2023-01-13 11:30:29 +0100

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,112 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "no.nav.syfo"
+version = "1.0.0"
+
+val coroutinesVersion = "1.6.4"
+val jacksonVersion = "2.14.1"
+val kluentVersion = "1.72"
+val ktorVersion = "2.2.2"
+val logbackVersion = "1.4.5"
+val logstashEncoderVersion = "7.2"
+val prometheusVersion = "0.16.0"
+val smCommonVersion = "1.d6548c5"
+val mockkVersion = "1.13.2"
+val testContainerKafkaVersion = "1.17.6"
+val kotlinVersion = "1.8.0"
+val kotestVersion = "5.5.4"
+
+tasks.withType<Jar> {
+    manifest.attributes["Main-Class"] = "no.nav.syfo.BootstrapKt"
+}
+
+plugins {
+    id("org.jmailen.kotlinter") version "3.12.0"
+    kotlin("jvm") version "1.8.0"
+    id("com.diffplug.spotless") version "6.11.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+buildscript {
+    dependencies {
+    }
+}
+
+val githubUser: String by project
+val githubPassword: String by project
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/syfosm-common")
+        credentials {
+            username = githubUser
+            password = githubPassword
+        }
+    }
+}
+
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:$coroutinesVersion")
+    implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
+    implementation("io.prometheus:simpleclient_common:$prometheusVersion")
+
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-id:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-apache:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+
+    testImplementation("org.amshove.kluent:kluent:$kluentVersion") 
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("org.testcontainers:kafka:$testContainerKafkaVersion")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
+        exclude(group = "org.eclipse.jetty") 
+    }
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+}
+
+tasks {
+
+    create("printVersion") {
+        println(project.version)
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    withType<ShadowJar> {
+        transform(ServiceFileTransformer::class.java) {
+            setPath("META-INF/cxf")
+            include("bus-extensions.txt")
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform {
+        }
+        testLogging.showStandardStreams = true
+    }
+
+    "check" {
+        dependsOn("formatKotlin")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "syk-dig-oppgavelytter"
