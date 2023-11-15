838fbefbdb28d4a9ef88665a740ad5fec048cb3f
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-11-15 14:56:25 +0100

    Bumped some dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,24 +4,25 @@ version = "1.0.0"
 val coroutinesVersion= "1.7.3"
 val jacksonVersion = "2.15.3"
 val kluentVersion = "1.73"
-val ktorVersion = "2.3.5"
+val ktorVersion = "2.3.6"
 val logbackVersion = "1.4.11"
 val logstashEncoderVersion = "7.4"
 val prometheusVersion = "0.16.0"
-val smCommonVersion = "2.0.4"
+val smCommonVersion = "2.0.6"
 val mockkVersion = "1.13.8"
-val kotlinVersion = "1.9.10"
+val kotlinVersion = "1.9.20"
 val postgresVersion = "42.6.0"
 val hikariVersion = "5.0.1"
 val googlePostgresVersion = "1.14.1"
 val flywayVersion = "9.22.3"
 val ktfmtVersion = "0.44"
-val junitJupiterVersion = "5.10.0"
+val junitJupiterVersion = "5.10.1"
+val commonsCodecVersion="1.16.0"
 
 
 plugins {
     id("application")
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.20"
     id("com.diffplug.spotless") version "6.22.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
@@ -55,6 +56,11 @@ dependencies {
     implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-apache:$ktorVersion")
+    constraints {
+        implementation("commons-codec:commons-codec:$commonsCodecVersion") {
+            because("override transient from io.ktor:ktor-client-apache")
+        }
+    }
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
 
